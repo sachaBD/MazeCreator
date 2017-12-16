b28be0f5d55254329e717c84047f93f30e57e4e7
sacha <sacha.borowskidavies@gmail.com>
2017-12-16 23:57:58 +1000

    initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdlib.h>
+#include "Maze.h"
+
+Maze* create_maze(int height, int width) {
+    srand(time(NULL));
+
+    Maze maze = Maze(height, width);
+
+    maze.create_initial_path();
+
+    maze.print();
+
+    maze.create_other_paths();
+
+    maze.print();
+
+    return &maze;
+}
+
+int main(int argc, char** argv) {
+    char* endInt;
+    Maze* maze = create_maze(strtol(argv[1], &endInt, 10), strtol(argv[2], &endInt, 10));
+    std::fflush(stdout);
+
+    return 0;
+}
